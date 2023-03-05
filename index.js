a41b976ba03728dc0ef75f0c6cf5ea109314721e
Bob <teemu9598@gmail.com>
2023-03-05 16:08:50 +0200

    Add warning paragraph to password validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let passwordField = document.getElementById('password');
 let passwordCheckField = document.getElementById('password_check');
+let warningShown = false
 
 passwordField.addEventListener('change', () => {
     comparePasswords()
@@ -13,10 +14,22 @@ const comparePasswords = () => {
     if (passwordField.value !== passwordCheckField.value) {
         passwordField.style.borderColor = 'red';
         passwordCheckField.style.borderColor = 'red';
+
+        if (!warningShown) {
+            p = document.createElement('p');
+            p.setAttribute('id', 'pwdWarning')
+            p.innerHTML = '<p>Passwords do not match!</p>';
+            p.style.color = 'red';
+
+            document.getElementById('password_row').appendChild(p);
+            warningShown = true;
+        }
     }
 
     else {
         passwordField.style.borderColor = '';
         passwordCheckField.style.borderColor = '';
+        document.getElementById('pwdWarning').remove();
+        warningShown = false;
     }
 };
